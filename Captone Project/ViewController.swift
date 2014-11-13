6dcc644cc50ab3e12ecec7955d4bb9930b2ab300
Michael Sevy <michaelsevy@gmail.com>
2014-11-12 18:14:27 -0600

    updated loop

--- Captone Project/ViewController.swift
+++ Captone Project/ViewController.swift
@@ -18,18 +18,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let manager = AFHTTPRequestOperationManager()
+
         
         manager.GET( "http://intutvp.herokuapp.com/intutv/api/v1.0/search/twitter/foofighters",
             parameters: nil,
             success: {  (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-                println("JSON: " + responseObject.description)
+                println(responseObject.description)
+              
                 
-                if let dataArray = responseObject.valueForKey("data") as? [AnyObject]       {
+                if let dataArray = responseObject as? [AnyObject]       {
                     self.scrollView.contentSize = CGSizeMake(320, CGFloat(320*dataArray.count))
                     for var i = 0; i < dataArray.count; i++     {
                         let dataObject: AnyObject = dataArray[i]
-                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                        println(dataObject[1].valueForKeyPath("user.profile_background_image_url")!)
+                        
+                        if let imageURLString = dataObject[1].valueForKeyPath("user.profile_image_url") as? String {
                             println("image " + String(i) + " URL is " + imageURLString)
+                            
+                            
                             
                             let imageData =  NSData(contentsOfURL: NSURL(string: imageURLString)!)
                             let imageView = UIImageView(image: UIImage(data: imageData!))
@@ -40,7 +46,7 @@ class ViewController: UIViewController {
                 }
                 
             },
-            
+
             failure:    {   (operation: AFHTTPRequestOperation!,error: NSError!) in
                 println("Error: " + error.localizedDescription)
         })
@@ -52,9 +58,12 @@ class ViewController: UIViewController {
     }
     
 }
+
 //Instagram JSON: https://api.instagram.com/v1/tags/"timberlake"/media/recent?client_id=952d6ff443ce448d90d3bf12d819b88b
 
 //Heroku JSON: http://intutvp.herokuapp.com/intutv/api/v1.0/search/twitter/foofighters
+
+//entities.urls.expanded_url
 
 //pulls JSON and inserts it in a label.text box knowm here as "username":
 //if let myName = responseObject.valueForKey("name") as? String {
